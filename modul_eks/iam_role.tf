77b430b0651e1d228cfe51e47b8a8f7aa833207f
anzhela-iondem <77322676+anzhela-iondem@users.noreply.github.com>
2024-07-02 11:54:48 +0200

    Update iam_role.tf. Make RDS read and write policy.

--- modul_eks/iam_role.tf
+++ modul_eks/iam_role.tf
@@ -35,13 +35,41 @@ resource "aws_iam_role_policy_attachment" "ec2_read_only_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "rds_read_only_access" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
-}
-
 resource "aws_iam_instance_profile" "eks_node_instance_profile" {
   name = "eks-node-instance-profile"
   role = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "rds_access_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = aws_iam_policy.rds_access_policy.arn
+}
+
+resource "aws_iam_policy" "rds_access_policy" {
+  name        = "RDSAccessPolicy"
+  description = "Provides read and write access to RDS resources"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "rds:DescribeDBInstances",
+          "rds:DescribeDBClusters",
+          "rds:CreateDBInstance",
+          "rds:ModifyDBInstance",
+          "rds:DeleteDBInstance",
+          "rds:CreateDBCluster",
+          "rds:ModifyDBCluster",
+          "rds:DeleteDBCluster",
+          "rds:ListTagsForResource",
+          "rds:AddTagsToResource",
+          "rds:RemoveTagsFromResource"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
 
